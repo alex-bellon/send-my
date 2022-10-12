8ed4e078112ff670287ba0fe5a583de1738cdb67
alwyen <alyen@ucsd.edu>
2022-10-12 14:47:21 -0700

    new branch for mac-side measurements

--- DataFetcher/OFFetchReports/OFFetchReportsMainView.swift
+++ DataFetcher/OFFetchReports/OFFetchReportsMainView.swift
@@ -54,28 +54,31 @@ struct OFFetchReportsMainView: View {
             let timeInterval = NSDate().timeIntervalSince1970
             print("Unix Time: \(timeInterval)")
             
-//            generating a bunch of keys
-            let staticPrefix: [UInt8] = [0xba, 0xbe]
-            let zeroPadding: [UInt8] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-            var count: UInt16 = 0
-            var validCounter: UInt8 = 0
-            var keyTest = [UInt8]()
-            while count < 65535 {
-                keyTest = staticPrefix + byteArray(from: modemID)
-                keyTest += byteArray(from: validCounter) + zeroPadding + byteArray(from: count)
-                var hexKeyTest = String(format:"%02X", keyTest[0])
-                for i in 1..<keyTest.count{
-                    hexKeyTest += " " + String(format:"%02X", keyTest[i])
-                }
-                print(hexKeyTest)
-                if BoringSSL.isPublicKeyValid(Data(keyTest)) == 0 {
-                    validCounter += 1
-                } else {
-                    count += 1
-                    validCounter = 0
+            let genKeys = false
+            
+            if genKeys {
+//                generating a bunch of keys
+                let staticPrefix: [UInt8] = [0xba, 0xbe]
+                let zeroPadding: [UInt8] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+                var count: UInt16 = 0
+                var validCounter: UInt8 = 0
+                var keyTest = [UInt8]()
+                while count < 65535 {
+                    keyTest = staticPrefix + byteArray(from: modemID)
+                    keyTest += byteArray(from: validCounter) + zeroPadding + byteArray(from: count)
+                    var hexKeyTest = String(format:"%02X", keyTest[0])
+                    for i in 1..<keyTest.count{
+                        hexKeyTest += " " + String(format:"%02X", keyTest[i])
+                    }
+                    print(hexKeyTest)
+                    if BoringSSL.isPublicKeyValid(Data(keyTest)) == 0 {
+                        validCounter += 1
+                    } else {
+                        count += 1
+                        validCounter = 0
+                    }
                 }
             }
-        
         },
         label: {
           Text("Download data")
